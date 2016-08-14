3033abbf90b032738344589bb332886ec203d972
Alexander <info@devsome.com>
2016-08-14 15:43:24 +0200

    mod kick plugin enabled

---
+++ bot/modules_mod/help.js
@@ -0,0 +1,57 @@
+/**
+  * This is the mod Plugin
+  * Using it will show the user all the commands we got
+  */
+const config  = require("./../../config/config.json");
+
+var mod = {
+  name: "help",
+  enabled: true,
+  on: ["help"],
+  usage: "",
+  description: "Telling you the truth about the mod bot commands",
+  cooldown: 10,
+  process: function(clientBot, msg, suffix) {
+    let toSend = [];
+    if (!suffix) {
+      toSend.push("Use `" + config.mod_command_prefix + "help <command name>` to get more info on a mod command.\n");
+      toSend.push("Mod commands you can use:```glsl\n");
+      Object.keys(commands_mod).forEach(cmd => {
+        if (!commands_mod[cmd].hasOwnProperty("shouldDisplay") || (commands_mod[cmd].hasOwnProperty("shouldDisplay") && commands_mod[cmd].shouldDisplay))
+          toSend.push("\n" + config.mod_command_prefix + commands_mod[cmd].on[0] + " " + commands_mod[cmd].usage + "\n\t#" + commands_mod[cmd].description);
+      });
+      toSend = toSend.join('');
+      if (toSend.length >= 1990) {
+        clientBot.sendMessage(msg.author, toSend.substr(0, 1990).substr(0, toSend.substr(0, 1990).lastIndexOf('\n\t')) + "```"); //part 1
+        setTimeout(() => {clientBot.sendMessage(msg.author, "```glsl" + toSend.substr(toSend.substr(0, 1990).lastIndexOf('\n\t')) + "```");}, 1000); //2
+      } else clientBot.sendMessage(msg.author, toSend + "```");
+    } else {
+      suffix = suffix.toLowerCase();
+      var buff;
+      for (var i = 0; i < commands_mod.length; i++) {
+        if(commands_mod[i].on.indexOf(suffix) > -1) {
+          buff = commands_mod[i];
+        }
+      }
+      if (buff) {
+        toSend.push("Mod command ``" + config.mod_command_prefix + suffix + "``\n```glsl\n");
+        if (buff.description) toSend.push("Description:# " + buff.description);
+        if (buff.usage) toSend.push("\nUsage:# " + buff.usage);
+        if (buff.cooldown) toSend.push("\nCooldown:# " + buff.cooldown + " seconds");
+        if (buff.on) toSend.push("\nShortcuts:# [ " + buff.on + " ] can be used");
+        if (buff.by) toSend.push("\nCoded by:# [" + buff.by + "]");
+        toSend = toSend.join('');
+        clientBot.sendMessage(msg, toSend + "\n```");
+      } else {
+        clientBot.sendMessage(msg, "Command `" + suffix + "` not found.", (erro, wMessage) => { clientBot.deleteMessage(wMessage, {"wait": 10000}); });
+      }
+    }
+  }
+};
+
+function findShortcuts(short) {
+  console.log(short.on);
+  return short.on === short.on;
+}
+
+module.exports = mod;
---
+++ bot/modules_mod/kick.js
@@ -0,0 +1,32 @@
+/**
+  * This is the mod kick Plugin
+  */
+
+var mod = {
+  name: "kick",
+  enabled: true,
+  on: ["kick", "getout"],
+  usage: "<@users> [message]",
+  description: "Kick a user with a message",
+  cooldown: 5,
+  by: "Devsome",
+  process: function(clientBot, msg, suffix) {
+    if (msg.channel.isPrivate) return;
+    if (!msg.channel.permissionsOf(msg.author).hasPermission("kickMembers") && !config.admin_id.includes(msg.author.id))
+      clientBot.sendMessage(msg, "You don't have permission", (e, m) => { bot.deleteMessage(m, {"wait": 10000}); });
+    else if (!msg.channel.permissionsOf(clientBot.user).hasPermission("kickMembers"))
+      clientBot.sendMessage(msg, "I don't have permission", (e, m) => { clientBot.deleteMessage(m, {"wait": 10000}); });
+    else if (suffix && msg.mentions.length > 0) {
+      let kickMessage = suffix.replace(/<@\d+>/g, "").trim();
+      msg.mentions.map(unlucky => {
+        if (!kickMessage) msg.channel.server.kickMember(unlucky);
+        else {
+          clientBot.sendMessage(unlucky, "You were kicked from " + msg.channel.server.name + " for reason: " + kickMessage).then(() => msg.channel.server.kickMember(unlucky));
+        }
+      });
+      clientBot.sendMessage(msg, msg.author.username + " 👌🏻", (e, m) => { clientBot.deleteMessage(m, {"wait": 10000}); });
+    } else utils.correctUsage("kick", this.usage, msg, clientBot, config.mod_command_prefix);
+  }
+};
+
+module.exports = mod;
