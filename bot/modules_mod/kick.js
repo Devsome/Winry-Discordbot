22cb8a9dee68619ac9a88d1db9787b7dba137345
Devsome <info@devsome.com>
2016-08-24 01:45:59 +0200

    fixed kick plugin -.-'

--- bot/modules_mod/kick.js
+++ bot/modules_mod/kick.js
@@ -2,6 +2,7 @@
   * This is the mod kick Plugin
   */
 const config  = require("./../../config/config.json");
+const utils  = require("./../../bot/utils.js");
 
 var mod = {
   name: "kick",
@@ -13,16 +14,16 @@ var mod = {
   by: "Devsome",
   deleteCommand: true,
   process: function(clientBot, msg, suffix) {
-    if (!msg.channel.permissionsOf(msg.author).hasPermission("kickMembers"))
-      clientBot.sendMessage(msg, "You don't have permission", (e, m) => { bot.deleteMessage(m, {"wait": 10000}); });
+    if (!msg.channel.permissionsOf(msg.author).hasPermission("kickMembers") && !config.admin_id.includes(msg.author.id))
+      clientBot.sendMessage(msg, "You don't have permission", (e, m) => { clientBot.deleteMessage(m, {"wait": 10000}); });
     else if (!msg.channel.permissionsOf(clientBot.user).hasPermission("kickMembers"))
       clientBot.sendMessage(msg, "I don't have permission", (e, m) => { clientBot.deleteMessage(m, {"wait": 10000}); });
     else if (suffix && msg.mentions.length > 0) {
       let kickMessage = suffix.replace(/<@\d+>/g, "").trim();
       msg.mentions.map(unlucky => {
-        msg.channel.server.kickMember(unlucky);
-        if (kickMessage)
-          clientBot.sendMessage(unlucky, "You were kicked from " + msg.channel.server.name + " for reason: " + kickMessage);
+        if (!kickMessage) msg.channel.server.kickMember(unlucky);
+        else {
+          clientBot.sendMessage(unlucky, "You were kicked from " + msg.channel.server.name + " for reason: " + kickMessage).then(() => msg.channel.server.kickMember(unlucky));
         }
       });
       clientBot.sendMessage(msg, msg.author.username + " 👌🏻", (e, m) => { clientBot.deleteMessage(m, {"wait": 10000}); });
